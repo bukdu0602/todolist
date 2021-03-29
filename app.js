ac4980306a40ac89716d9c383610ca09940afce1
Ryan Lim <ryan.hyun@gmail.com>
2021-03-28 19:00:09 -0700

    second

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 //jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const app = express();
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+
+let items = [];
+
 
 app.get("/", function(req, res){
   let today = new Date();
-  let currentDay = today.getDay();
-  let daysinArray = ["Sunday", "Monday", "Tuesday", "Wedsneday", "Thursday", "Friday", "Saturday"];
-  let newDay = daysinArray[currentDay];
-  res.render("list", {kindOfDay:newDay});
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+let day = today.toLocaleDateString("en-US", options);
 
+  res.render("list", {kindOfDay:day, newListItem: items});
 });
+
+
+app.post("/", function(req, res){
+  let item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
+});
+
+
 
 app.listen(3000, function(){
   console.log("Server started on port 3000");
